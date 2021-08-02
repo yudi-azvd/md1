1082f2cef8c5766d3500a4f0f6ddf7385c9c5cc8
Yudi Yamane <yudiyamane@gmail.com>
2021-08-01 21:52:56 -0300

    another exercise

--- 01-mathematical-induction/induction.py
+++ 01-mathematical-induction/induction.py
@@ -1,10 +1,22 @@
 
-n = 10
+# n = 10
+# partial_sum = 0
+
+# # \Sigma 4i-3 = n(2n-1)
+# for i in range(1, n+1):
+#   partial_sum += 4*i-3
+
+# print(f'partial sum = {partial_sum}')
+# print(f'    n(2n-1) = {n*(2*n-1)}')
+
+n = 998
 partial_sum = 0
 
 # \Sigma 4i-3 = n(2n-1)
 for i in range(1, n+1):
-  partial_sum += 4*i-3
+  print(2*i-1)
+  partial_sum += 2*i-1
 
 print(f'partial sum = {partial_sum}')
-print(f'    n(2n-1) = {n*(2*n-1)}')
+print(f'         n² = {n**2}')
+
